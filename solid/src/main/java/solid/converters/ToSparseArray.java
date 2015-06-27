871e58a63df92b2c6f60880d73573b0aed5aef6a
konmik <morelk@gmail.com>
2015-06-28 02:54:27 +0300

    javadoc update

--- solid/src/main/java/solid/converters/ToSparseArray.java
+++ solid/src/main/java/solid/converters/ToSparseArray.java
@@ -6,12 +6,32 @@ import solid.functions.SolidFunc1;
 
 public class ToSparseArray<T> implements SolidFunc1<Iterable<T>, SparseArray<T>> {
 
-    public static <T> ToSparseArray<T> toSparseArray(SolidFunc1<T, Integer> itemToKey, int initialCapacity) {
-        return new ToSparseArray<>(itemToKey, initialCapacity);
-    }
-
+    /**
+     * Returns a method that can be used with {@link solid.stream.Stream#collect(SolidFunc1)}
+     * to convert a stream into a {@link SparseArray}.
+     *
+     * @param <T>       a type of stream items.
+     * @param itemToKey a method that should return a key for an item.
+     * @return a method that converts an iterable into a {@link SparseArray}.
+     */
     public static <T> ToSparseArray<T> toSparseArray(SolidFunc1<T, Integer> itemToKey) {
         return new ToSparseArray<>(itemToKey);
+    }
+
+    /**
+     * Returns a method that can be used with {@link solid.stream.Stream#collect(SolidFunc1)}
+     * to convert a stream into a {@link SparseArray}.
+     * <p/>
+     * Use this method instead of {@link #toSparseArray(SolidFunc1)}} for better performance on
+     * streams that can have more than 10 items.
+     *
+     * @param <T>             a type of stream items.
+     * @param itemToKey       a method that should return a key for an item.
+     * @param initialCapacity initial capacity on the sparse array.
+     * @return a method that converts an iterable into a {@link SparseArray}.
+     */
+    public static <T> ToSparseArray<T> toSparseArray(SolidFunc1<T, Integer> itemToKey, int initialCapacity) {
+        return new ToSparseArray<>(itemToKey, initialCapacity);
     }
 
     private SolidFunc1<T, Integer> getItemKey;
