2f87d5f6d810df4032c1a07851879ee786afeb14
konmik <morelk@gmail.com>
2015-11-17 21:28:10 +0300

    optional

---
+++ solid-streams/src/main/java/solid/functions/SolidFunc0.java
@@ -0,0 +1,5 @@
+package solid.functions;
+
+public interface SolidFunc0<R> {
+    R call();
+}
---
+++ solid-streams/src/test/java/solid/optional/OptionalTest.java
@@ -0,0 +1,92 @@
+package solid.optional;
+
+import org.junit.Test;
+
+import solid.functions.SolidAction1;
+import solid.functions.SolidFunc0;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class OptionalTest {
+
+    @Test
+    public void testNull() throws Exception {
+        Optional<Object> o = Optional.of(null);
+
+        assertFalse(o.isPresent());
+
+        SolidAction1 mockAction = mock(SolidAction1.class);
+        o.ifPresent(mockAction);
+        verifyNoMoreInteractions(mockAction);
+
+        assertEquals(1, o.or(1));
+        assertEquals(1, o.or(new SolidFunc0<Object>() {
+            @Override
+            public Object call() {
+                return 1;
+            }
+        }));
+        assertNull(o.orNull());
+
+        assertTrue(o.equals(Optional.empty()));
+
+        assertEquals(o.hashCode(), Optional.empty().hashCode());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullNPE() throws Exception {
+        Optional.of(null).get();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testNullOrThrow() throws Exception {
+        Optional<Object> o = Optional.of(null);
+
+        o.orThrow(new SolidFunc0<RuntimeException>() {
+            @Override
+            public RuntimeException call() {
+                return new RuntimeException();
+            }
+        });
+    }
+
+    @Test
+    public void testValue() throws Exception {
+        Optional<Object> o = Optional.<Object>of(1);
+
+        assertTrue(o.isPresent());
+
+        SolidAction1<Object> mockAction = mock(SolidAction1.class);
+        o.ifPresent(mockAction);
+        verify(mockAction, times(1)).call(eq(1));
+
+        assertEquals(1, o.or(2));
+        assertEquals(1, o.or(new SolidFunc0<Object>() {
+            @Override
+            public Object call() {
+                return 2;
+            }
+        }));
+        assertEquals(1, o.orNull());
+
+        assertTrue(Optional.of(1).equals(o));
+
+        assertEquals(o.hashCode(), Optional.of(1).hashCode());
+
+        assertEquals(1, o.get());
+        assertEquals(1, o.orThrow(new SolidFunc0<RuntimeException>() {
+            @Override
+            public RuntimeException call() {
+                return new RuntimeException();
+            }
+        }));
+    }
+}
